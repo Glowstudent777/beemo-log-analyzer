ef41d929edc552bd8afc7bb43131b393a0c6ada3
Glowstudent <cameronmenken7@gmail.com>
2023-11-29 19:03:44 -0600

    Added some stuff

---
+++ Beemologanalyzer/Parser.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Beemologanalyzer;
+
+public class Parser
+{
+	public static int ParseJoinDates(string input)
+	{
+		string pattern = @"/(\\d\\d:\\d\\d:\\d\\d\\.\\d\\d\\d[+,-]\\d\\d\\d\\d)/w+";
+
+		foreach (Match match in Regex.Matches(input, pattern, RegexOptions.IgnoreCase))
+			Console.WriteLine("{0}\n{1}\n{2}", match.Value, match.Groups[1].Value, match.Index);
+
+		int length = input.Length;
+		Console.WriteLine(length);
+		return length;
+	}
+}
---
+++ Benches/Benchmark.cs
@@ -0,0 +1,25 @@
+﻿using Beemologanalyzer;
+using System;
+using System.IO;
+
+namespace Benches
+{
+	public class Benchmark
+	{
+		public static async Task Main(string[] args)
+		{
+			try
+			{
+				using (var sr = new StreamReader("log_data.txt"))
+				{
+					Parser.ParseJoinDates(sr.ReadToEnd());
+				}
+			}
+			catch (IOException e)
+			{
+				Console.WriteLine("The file could not be read:");
+				Console.WriteLine(e.Message);
+			}
+		}
+	}
+}
